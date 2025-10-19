32a32e164486e1ef79e18fa76576e5a90d2c1b9a
yannicksuter <yannick.suter@gmail.com>
2025-10-19 22:39:33 +0200

    first pass on simpleparser.. what the heck, is this logic

--- include/Game/Sys/simpleparser.h
+++ include/Game/Sys/simpleparser.h
@@ -10,21 +10,14 @@ public:
     bool StartParsing(char*, int, bool);
     char* NextToken(bool);
     char* NextTokenOnLine(bool);
-    void AdvanceLine();
+    bool AdvanceLine();
 
-    /* 0x000 */ s8 m_TokenBuffer[5][256];
-    /* 0x500 */ s8* m_CurrPos;
-    /* 0x504 */ s8* m_EndOfLine;
-    /* 0x508 */ s32 m_AmountLeft;
-    /* 0x50C */ s32 m_CurrentBuffer;
+    /* 0x000 */ char m_TokenBuffer[5][256];
+    /* 0x500 */ char* m_CurrPos;
+    /* 0x504 */ char* m_EndOfLine;
+    /* 0x508 */ int m_AmountLeft;
+    /* 0x50C */ int m_CurrentBuffer;
     /* 0x510 */ bool m_bSpacesAreWhitespace;
-
-    // char m_TokenBuffer[5][256];  // [0x000..0x4FF]
-    // char* m_CurrPos;             // [0x500] current position within line (token cursor)
-    // char* m_EndOfLine;           // [0x504] current line end sentinel (points at '\n' or scan head)
-    // int m_AmountLeft;            // [0x508] bytes left from mLineEnd to end of buffer
-    // int m_CurrentBuffer;         // [0x50C] rotating token buffer index 0..4
-    // bool m_bSpacesAreWhitespace; // [0x510] 1 => treat ' ' as whitespace; 0 => keep spaces in tokens
 }; // size: 0x514
 
 #endif // _SIMPLEPARSER_H_
--- src/Game/Sys/simpleparser.cpp
+++ src/Game/Sys/simpleparser.cpp
@@ -1,5 +1,7 @@
 #include "Game/Sys/simpleparser.h"
 
+#include "ctype.h"
+
 /**
  * Offset/Address/Size: 0x988 | 0x801D732C | size: 0x10
  */
@@ -12,9 +14,117 @@ SimpleParser::SimpleParser()
 /**
  * Offset/Address/Size: 0x81C | 0x801D71C0 | size: 0x16C
  */
-bool SimpleParser::StartParsing(char*, int, bool)
+bool SimpleParser::StartParsing(char* data, int size, bool bSpacesAreWhitespace)
 {
-    return false;
+    unsigned char isWhitespace;
+    char currentChar;
+    char* lineStart;
+
+    m_EndOfLine = data;
+    m_AmountLeft = size;
+    m_bSpacesAreWhitespace = bSpacesAreWhitespace;
+
+    if (m_AmountLeft <= 1)
+        return false;
+
+restart:
+    for (;;)
+    {
+        isWhitespace = 0;
+        lineStart = m_EndOfLine;
+        currentChar = *lineStart;
+        int charValue = (s8) * (u8*)lineStart;
+        if ((__ctype_map[(u8)charValue] & 0x6) != 0)
+        {
+            if (m_bSpacesAreWhitespace || charValue != 0x20)
+                isWhitespace = 1;
+        }
+
+        if (!isWhitespace)
+            break;
+
+        unsigned char canAdvance;
+        if (m_AmountLeft <= 1)
+        {
+            canAdvance = 0;
+        }
+        else
+        {
+            canAdvance = 1;
+            m_EndOfLine += 1;
+            m_AmountLeft -= 1;
+        }
+
+        if (canAdvance == 0)
+        {
+            return false;
+        }
+    }
+
+    if ((int)(signed char)currentChar == 0x23)
+    {
+        for (;;)
+        {
+            unsigned char hasAdvanced;
+            if (m_AmountLeft <= 1)
+            {
+                hasAdvanced = 0;
+            }
+            else
+            {
+                hasAdvanced = 1;
+                m_EndOfLine += 1;
+                m_AmountLeft -= 1;
+            }
+
+            unsigned char foundNewline;
+            if (hasAdvanced == 0)
+            {
+                foundNewline = 0;
+            }
+            else
+            {
+                if (*m_EndOfLine == '\n')
+                {
+                    foundNewline = 1;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (foundNewline == 0)
+                return false;
+
+            goto restart; // I am sure this was not in the original code, but it is here to make the assembly match.
+        }
+    }
+
+    m_CurrPos = lineStart;
+
+    for (;;)
+    {
+        unsigned char canStep;
+        if (m_AmountLeft <= 1)
+        {
+            canStep = 0;
+        }
+        else
+        {
+            canStep = 1;
+            m_EndOfLine += 1;
+            m_AmountLeft -= 1;
+        }
+
+        if (!canStep)
+            break;
+
+        if (*m_EndOfLine == '\n')
+            break;
+    }
+
+    return true;
 }
 
 /**
@@ -36,6 +146,112 @@ char* SimpleParser::NextTokenOnLine(bool)
 /**
  * Offset/Address/Size: 0x0 | 0x801D69A4 | size: 0x160
  */
-void SimpleParser::AdvanceLine()
+bool SimpleParser::AdvanceLine()
 {
+    unsigned char skip;
+    unsigned char step;
+    char cur;
+    char* p;
+
+    if (m_AmountLeft <= 1)
+        return false;
+
+    unsigned char* ctype = __ctype_map;
+
+restart:
+    for (;;)
+    {
+        skip = 0;
+        p = m_EndOfLine;
+        cur = *p;
+
+        int ch = (signed char)(*(unsigned char*)p);
+
+        if ((ctype[(unsigned char)ch] & 0x06) != 0)
+        {
+            if (m_bSpacesAreWhitespace || ch != 0x20)
+                skip = 1;
+        }
+
+        if (!skip)
+            break;
+
+        if (m_AmountLeft <= 1)
+        {
+            step = 0;
+        }
+        else
+        {
+            step = 1;
+            m_EndOfLine += 1;
+            m_AmountLeft -= 1;
+        }
+
+        if (step == 0)
+            return false;
+    }
+
+    if ((int)(signed char)cur == 0x23)
+    {
+        for (;;)
+        {
+            unsigned char hasAdvanced;
+            if (m_AmountLeft <= 1)
+            {
+                hasAdvanced = 0;
+            }
+            else
+            {
+                hasAdvanced = 1;
+                m_EndOfLine += 1;
+                m_AmountLeft -= 1;
+            }
+
+            unsigned char foundNewline;
+            if (hasAdvanced == 0)
+            {
+                foundNewline = 0;
+            }
+            else
+            {
+                if (*m_EndOfLine == '\n')
+                {
+                    foundNewline = 1;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (foundNewline == 0)
+                return false;
+
+            goto restart; // I am sure this was not in the original code, but it is here to make the assembly match.
+        }
+    }
+
+    m_CurrPos = p;
+
+    for (;;)
+    {
+        if (m_AmountLeft <= 1)
+        {
+            step = 0;
+        }
+        else
+        {
+            step = 1;
+            m_EndOfLine += 1;
+            m_AmountLeft -= 1;
+        }
+
+        if (!step)
+            break;
+
+        if (*m_EndOfLine == '\n')
+            break;
+    }
+
+    return true;
 }
