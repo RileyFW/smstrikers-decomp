e873947a79aa172598bd8ad136a6003a22757c49
Yannick Suter <yannick.suter@gmail.com>
2026-03-07 00:04:24 +0100

    Update SHierarchy.cpp

--- src/Game/SHierarchy.cpp
+++ src/Game/SHierarchy.cpp
@@ -22,7 +22,7 @@ nlVector3* cSHierarchy::GetTranslationOffset(int nodeIndex) const
  */
 s32 cSHierarchy::GetParent(int nodeIndex) const
 {
-    return *((s32*)this->m_parentIndices + nodeIndex);
+    return m_parentIndices[nodeIndex];
 }
 
 /**
@@ -30,7 +30,7 @@ s32 cSHierarchy::GetParent(int nodeIndex) const
  */
 s32 cSHierarchy::GetPushPop(int nodeIndex) const
 {
-    return *((s32*)this->m_pushPopFlags + nodeIndex);
+    return m_pushPopFlags[nodeIndex];
 }
 
 /**
@@ -38,7 +38,7 @@ s32 cSHierarchy::GetPushPop(int nodeIndex) const
  */
 s32 cSHierarchy::GetMirroredNode(int nodeIndex) const
 {
-    return *((s32*)this->m_mirroredNodeIndices + nodeIndex);
+    return m_mirroredNodeIndices[nodeIndex];
 }
 
 /**
@@ -82,362 +82,199 @@ s32 cSHierarchy::GetNodeIndexByID(unsigned int nodeID) const
  */
 s32 cSHierarchy::GetChild(int parentIndex, int childIndex) const
 {
-    return *((s32*)((s32**)this->m_childArrays)[parentIndex] + childIndex);
+    FORCE_DONT_INLINE;
+    return m_childArrays[parentIndex][childIndex];
 }
 
+// NONMATCHING - regalloc
 /**
  * Offset/Address/Size: 0xD0 | 0x801EE0BC | size: 0x284
  */
 void cSHierarchy::BuildPushPopFlags(int nodeIndex, int currentDepth, int& stackDepth)
 {
-    s32 nodeOffset;
-    s32 childCount;
-    s32 temp_r0;
-    s32 temp_r0_2;
-    s32 temp_r0_3;
-    s32 newStackDepth;
-    s32 childNodeIndex;
-    s32 grandchildIndex;
-    s32 grandchildStackDepth;
-    s32 grandchildNodeIndex;
-    s32 grandchildChildCount;
-    s32 greatGrandchildStackDepth;
-    s32 greatGrandchildChildCount;
-    s32 greatGrandchildIndex;
-    s32 greatGrandchildNodeIndex;
-    s32 temp_r4;
-    s32 temp_r4_2;
-    s32 previousStackDepth;
-    s32 previousStackDepth_2;
-    s32 temp_r5;
-    s32 temp_r5_2;
-    s32 temp_r6;
-    s32 temp_r6_2;
-    s32 childArrayOffset;
-    s32 childLoopCounter;
-    s32 grandchildArrayOffset;
-    s32 grandchildLoopCounter;
-    s32 greatGrandchildLoopCounter;
+    int l;
+    int newSD3;
+    int cc3;
+    int ggchild;
+    int k;
+    int newSD2;
+    int cc2;
+    int gchild;
+    int j;
+    int newSD1;
+    int cc1;
+    int child;
+    int i;
+    int newSD0;
+    int childCount;
 
-    temp_r6 = stackDepth;
-    if (currentDepth != temp_r6)
+    if (currentDepth != stackDepth)
     {
-        ((s32*)this->m_pushPopFlags)[nodeIndex - 1] = currentDepth - temp_r6;
+        m_pushPopFlags[nodeIndex - 1] = currentDepth - stackDepth;
         stackDepth = currentDepth;
     }
-    temp_r0 = nodeIndex * 4;
-    nodeOffset = temp_r0;
-    temp_r0_2 = ((s32*)this->m_childCounts)[nodeIndex];
-    childCount = temp_r0_2;
-    if (temp_r0_2 != 0)
+
+    childCount = m_childCounts[nodeIndex];
+    if (childCount != 0)
     {
-        childLoopCounter = 0;
-        childArrayOffset = 0;
-        ((s32*)this->m_pushPopFlags)[nodeIndex] = 1;
+        m_pushPopFlags[nodeIndex] = 1;
         stackDepth += 1;
-        newStackDepth = stackDepth;
-    loop_29:
-        if (childLoopCounter < childCount)
+        newSD0 = stackDepth;
+
+        for (i = 0; i < childCount; i++)
         {
-            temp_r5 = stackDepth;
-            temp_r6_2 = ((s32*)((s32**)this->m_childArrays)[nodeIndex])[childLoopCounter];
-            if (newStackDepth != temp_r5)
+            child = m_childArrays[nodeIndex][i];
+
+            if (newSD0 != stackDepth)
             {
-                ((s32*)this->m_pushPopFlags)[temp_r6_2 - 1] = newStackDepth - temp_r5;
-                stackDepth = newStackDepth;
+                m_pushPopFlags[child - 1] = newSD0 - stackDepth;
+                stackDepth = newSD0;
             }
-            childNodeIndex = temp_r6_2 * 4;
-            grandchildChildCount = ((s32*)this->m_childCounts)[temp_r6_2];
-            if (grandchildChildCount != 0)
+
+            cc1 = m_childCounts[child];
+            if (cc1 != 0)
             {
-                grandchildLoopCounter = 0;
-                ((s32*)this->m_pushPopFlags)[temp_r6_2] = 1;
-                grandchildArrayOffset = 0;
+                m_pushPopFlags[child] = 1;
                 stackDepth += 1;
-                greatGrandchildStackDepth = stackDepth;
-            loop_25:
-                if (grandchildLoopCounter < grandchildChildCount)
+                newSD1 = stackDepth;
+
+                for (j = 0; j < cc1; j++)
                 {
-                    temp_r5_2 = stackDepth;
-                    grandchildNodeIndex = ((s32*)((s32**)this->m_childArrays)[temp_r6_2])[grandchildLoopCounter];
-                    if (greatGrandchildStackDepth != temp_r5_2)
+                    gchild = m_childArrays[child][j];
+
+                    if (newSD1 != stackDepth)
                     {
-                        ((s32*)this->m_pushPopFlags)[grandchildNodeIndex - 1] = greatGrandchildStackDepth - temp_r5_2;
-                        stackDepth = greatGrandchildStackDepth;
+                        m_pushPopFlags[gchild - 1] = newSD1 - stackDepth;
+                        stackDepth = newSD1;
                     }
-                    temp_r4 = grandchildNodeIndex * 4;
-                    greatGrandchildChildCount = ((s32*)this->m_childCounts)[grandchildNodeIndex];
-                    if (greatGrandchildChildCount != 0)
+
+                    cc2 = m_childCounts[gchild];
+                    if (cc2 != 0)
                     {
-                        greatGrandchildLoopCounter = 0;
-                        ((s32*)this->m_pushPopFlags)[grandchildNodeIndex] = 1;
+                        m_pushPopFlags[gchild] = 1;
                         stackDepth += 1;
-                        greatGrandchildIndex = stackDepth;
-                    loop_21:
-                        if (greatGrandchildLoopCounter < greatGrandchildChildCount)
+                        newSD2 = stackDepth;
+
+                        for (k = 0; k < cc2; k++)
                         {
-                            temp_r0_3 = stackDepth;
-                            greatGrandchildNodeIndex = GetChild(grandchildNodeIndex, greatGrandchildLoopCounter);
-                            if (greatGrandchildIndex != temp_r0_3)
+                            ggchild = GetChild(gchild, k);
+
+                            if (newSD2 != stackDepth)
                             {
-                                ((s32*)this->m_pushPopFlags)[greatGrandchildNodeIndex - 1] = greatGrandchildIndex - temp_r0_3;
-                                stackDepth = greatGrandchildIndex;
+                                m_pushPopFlags[ggchild - 1] = newSD2 - stackDepth;
+                                stackDepth = newSD2;
                             }
-                            temp_r4_2 = greatGrandchildNodeIndex * 4;
-                            s32 greatGreatGrandchildChildCount = ((s32*)this->m_childCounts)[greatGrandchildNodeIndex];
-                            if (greatGreatGrandchildChildCount != 0)
+
+                            cc3 = m_childCounts[ggchild];
+                            if (cc3 != 0)
                             {
-                                s32 greatGreatGrandchildLoopCounter = 0;
-                                ((s32*)this->m_pushPopFlags)[greatGrandchildNodeIndex] = 1;
+                                m_pushPopFlags[ggchild] = 1;
                                 stackDepth += 1;
-                                s32 greatGreatGrandchildStackDepth = stackDepth;
-                            loop_17:
-                                if (greatGreatGrandchildLoopCounter < greatGreatGrandchildChildCount)
+                                newSD3 = stackDepth;
+
+                                for (l = 0; l < cc3; l++)
                                 {
-                                    BuildPushPopFlags(GetChild(greatGrandchildNodeIndex, greatGreatGrandchildLoopCounter),
-                                        greatGreatGrandchildStackDepth,
-                                        stackDepth);
-                                    greatGreatGrandchildLoopCounter += 1;
-                                    goto loop_17;
+                                    BuildPushPopFlags(GetChild(ggchild, l), newSD3, stackDepth);
                                 }
                             }
                             else
                             {
-                                ((s32*)this->m_pushPopFlags)[greatGrandchildNodeIndex] = 0;
+                                m_pushPopFlags[ggchild] = 0;
                             }
-                            greatGrandchildLoopCounter += 1;
-                            goto loop_21;
                         }
                     }
                     else
                     {
-                        ((s32*)this->m_pushPopFlags)[grandchildNodeIndex] = 0;
+                        m_pushPopFlags[gchild] = 0;
                     }
-                    grandchildArrayOffset += 4;
-                    grandchildLoopCounter += 1;
-                    goto loop_25;
                 }
             }
             else
             {
-                ((s32*)this->m_pushPopFlags)[temp_r6_2] = 0;
+                m_pushPopFlags[child] = 0;
             }
-            childArrayOffset += 4;
-            childLoopCounter += 1;
-            goto loop_29;
         }
-        return;
     }
-    ((s32*)this->m_pushPopFlags)[nodeIndex] = 0;
+    else
+    {
+        m_pushPopFlags[nodeIndex] = 0;
+    }
 }
 
+static inline void* nlGetChunkData(nlChunk* chunk)
+{
+    u32 alignField = chunk->m_ID & 0x7F000000;
+    u32 isAligned = ((-alignField) | alignField) >> 31;
+    if (isAligned != 0)
+    {
+        u32 alignment = 1u << (alignField >> 24);
+        u32 ptr = (u32)chunk + alignment;
+        ptr += 7;
+        ptr &= ~(alignment - 1);
+        return (void*)ptr;
+    }
+    return (void*)((u8*)chunk + 8);
+}
+
+static inline nlChunk* nlGetNextChunk(nlChunk* chunk)
+{
+    return (nlChunk*)((u8*)chunk + chunk->m_Size + 8);
+}
+
+// NONMATCHING - regalloc
 /**
  * Offset/Address/Size: 0x354 | 0x801EE340 | size: 0x3E0
  */
 cSHierarchy* cSHierarchy::Initialize(nlChunk* chunkData)
 {
-    s32 stackDepth;
-    void* chunkPtr;
-    void* chunkPtr_6;
-    s32 temp_r0;
-    s32 temp_r3;
-    s32 temp_r3_10;
-    s32 temp_r3_11;
-    s32 temp_r3_2;
-    s32 temp_r3_3;
-    s32 temp_r3_4;
-    s32 temp_r3_5;
-    s32 temp_r3_6;
-    s32 temp_r3_7;
-    s32 temp_r3_8;
-    s32 temp_r3_9;
-    s32 temp_r4;
-    s32 temp_r4_10;
-    s32 temp_r4_11;
-    s32 temp_r4_2;
-    s32 temp_r4_3;
-    s32 temp_r4_4;
-    s32 temp_r4_5;
-    s32 temp_r4_6;
-    s32 temp_r4_7;
-    s32 temp_r4_8;
-    s32 temp_r4_9;
-    s32 alignedOffset;
-    s32 alignedOffset_10;
-    s32 alignedOffset_11;
-    s32 alignedOffset_2;
-    s32 alignedOffset_3;
-    s32 alignedOffset_4;
-    s32 alignedOffset_5;
-    s32 alignedOffset_6;
-    s32 alignedOffset_7;
-    s32 alignedOffset_8;
-    s32 alignedOffset_9;
-    s32 nodeIndex;
-    s32 childArrayOffset;
-    s32 nodeCounter;
-    void* finalChunkPtr;
-    void* chunkPtr_2;
-    void* chunkPtr_3;
-    void* chunkPtr_4;
-    void* chunkPtr_5;
-    void* nextChunkPtr;
-    void* nextChunkPtr_2;
-    void* nextChunkPtr_3;
-    void* nextChunkPtr_4;
+    nlChunk* chunk = (nlChunk*)&m_nodeCount;
+    cSHierarchy* hier = (cSHierarchy*)nlGetChunkData(chunk);
 
-    chunkPtr = (void*)&this->m_nodeCount;
-    temp_r3 = this->m_nodeCount & 0x7F000000;
-    if (((-temp_r3 | temp_r3) >> 0x1F) != 0)
+    chunk = nlGetNextChunk(chunk);
+    hier->m_szName = (const char*)nlGetChunkData(chunk);
+
+    chunk = nlGetNextChunk(chunk);
+    hier->m_nodeIDs = (u32*)nlGetChunkData(chunk);
+
+    chunk = nlGetNextChunk(chunk);
+    hier->m_parentIndices = (s32*)nlGetChunkData(chunk);
+
+    chunk = nlGetNextChunk(chunk);
+    hier->m_childCounts = (s32*)nlGetChunkData(chunk);
+
+    chunk = nlGetNextChunk(chunk);
+    hier->m_childArrays = (s32**)nlGetChunkData(chunk);
+
+    chunk = nlGetNextChunk(chunk);
+    hier->m_pushPopFlags = (s32*)nlGetChunkData(chunk);
+
+    nlChunk* childDataChunk = nlGetNextChunk(chunk);
+    s32* childData = (s32*)nlGetChunkData(childDataChunk);
+
+    for (int i = 0; i < hier->m_nodeCount; i++)
     {
-        temp_r4 = 1 << (temp_r3 >> 0x18);
-        alignedOffset = (s32)((char*)chunkPtr + temp_r4 + 7) & ~(temp_r4 - 1);
-    }
-    else
-    {
-        alignedOffset = (s32)((char*)chunkPtr + 8);
-    }
-    nextChunkPtr = (void*)((char*)chunkPtr + (*(s32*)((char*)chunkPtr + 4) + 8));
-    temp_r3_2 = *(s32*)nextChunkPtr & 0x7F000000;
-    if (((-temp_r3_2 | temp_r3_2) >> 0x1F) != 0)
-    {
-        temp_r4_2 = 1 << (temp_r3_2 >> 0x18);
-        alignedOffset_2 = (s32)((char*)nextChunkPtr + temp_r4_2 + 7) & ~(temp_r4_2 - 1);
-    }
-    else
-    {
-        alignedOffset_2 = (s32)((char*)nextChunkPtr + 8);
-    }
-    ((cSHierarchy*)alignedOffset)->m_nodeIDs = (u32*)alignedOffset_2;
-    chunkPtr_2 = (void*)((char*)nextChunkPtr + (*(s32*)((char*)nextChunkPtr + 4) + 8));
-    temp_r3_3 = *(s32*)chunkPtr_2 & 0x7F000000;
-    if (((-temp_r3_3 | temp_r3_3) >> 0x1F) != 0)
-    {
-        temp_r4_3 = 1 << (temp_r3_3 >> 0x18);
-        alignedOffset_3 = (s32)((char*)chunkPtr_2 + temp_r4_3 + 7) & ~(temp_r4_3 - 1);
-    }
-    else
-    {
-        alignedOffset_3 = (s32)((char*)chunkPtr_2 + 8);
-    }
-    ((cSHierarchy*)alignedOffset)->m_nodeIDs = (u32*)alignedOffset_3;
-    nextChunkPtr_2 = (void*)((char*)chunkPtr_2 + (*(s32*)((char*)chunkPtr_2 + 4) + 8));
-    temp_r3_4 = *(s32*)nextChunkPtr_2 & 0x7F000000;
-    if (((-temp_r3_4 | temp_r3_4) >> 0x1F) != 0)
-    {
-        temp_r4_4 = 1 << (temp_r3_4 >> 0x18);
-        alignedOffset_4 = (s32)((char*)nextChunkPtr_2 + temp_r4_4 + 7) & ~(temp_r4_4 - 1);
-    }
-    else
-    {
-        alignedOffset_4 = (s32)((char*)nextChunkPtr_2 + 8);
-    }
-    ((cSHierarchy*)alignedOffset)->m_parentIndices = (s32*)alignedOffset_4;
-    chunkPtr_3 = (void*)((char*)nextChunkPtr_2 + (*(s32*)((char*)nextChunkPtr_2 + 4) + 8));
-    temp_r3_5 = *(s32*)chunkPtr_3 & 0x7F000000;
-    if (((-temp_r3_5 | temp_r3_5) >> 0x1F) != 0)
-    {
-        temp_r4_5 = 1 << (temp_r3_5 >> 0x18);
-        alignedOffset_5 = (s32)((char*)chunkPtr_3 + temp_r4_5 + 7) & ~(temp_r4_5 - 1);
-    }
-    else
-    {
-        alignedOffset_5 = (s32)((char*)chunkPtr_3 + 8);
-    }
-    ((cSHierarchy*)alignedOffset)->m_childCounts = (s32*)alignedOffset_5;
-    nextChunkPtr_3 = (void*)((char*)chunkPtr_3 + (*(s32*)((char*)chunkPtr_3 + 4) + 8));
-    temp_r3_6 = *(s32*)nextChunkPtr_3 & 0x7F000000;
-    if (((-temp_r3_6 | temp_r3_6) >> 0x1F) != 0)
-    {
-        temp_r4_6 = 1 << (temp_r3_6 >> 0x18);
-        alignedOffset_6 = (s32)((char*)nextChunkPtr_3 + temp_r4_6 + 7) & ~(temp_r4_6 - 1);
-    }
-    else
-    {
-        alignedOffset_6 = (s32)((char*)nextChunkPtr_3 + 8);
-    }
-    ((cSHierarchy*)alignedOffset)->m_childArrays = (s32**)alignedOffset_6;
-    chunkPtr_4 = (void*)((char*)nextChunkPtr_3 + (*(s32*)((char*)nextChunkPtr_3 + 4) + 8));
-    temp_r3_7 = *(s32*)chunkPtr_4 & 0x7F000000;
-    if (((-temp_r3_7 | temp_r3_7) >> 0x1F) != 0)
-    {
-        temp_r4_7 = 1 << (temp_r3_7 >> 0x18);
-        alignedOffset_7 = (s32)((char*)chunkPtr_4 + temp_r4_7 + 7) & ~(temp_r4_7 - 1);
-    }
-    else
-    {
-        alignedOffset_7 = (s32)((char*)chunkPtr_4 + 8);
-    }
-    ((cSHierarchy*)alignedOffset)->m_pushPopFlags = (s32*)alignedOffset_7;
-    finalChunkPtr = (void*)((char*)chunkPtr_4 + (*(s32*)((char*)chunkPtr_4 + 4) + 8));
-    temp_r3_8 = *(s32*)finalChunkPtr & 0x7F000000;
-    if (((-temp_r3_8 | temp_r3_8) >> 0x1F) != 0)
-    {
-        temp_r4_8 = 1 << (temp_r3_8 >> 0x18);
-        alignedOffset_8 = (s32)((char*)finalChunkPtr + temp_r4_8 + 7) & ~(temp_r4_8 - 1);
-    }
-    else
-    {
-        alignedOffset_8 = (s32)((char*)finalChunkPtr + 8);
-    }
-    nodeIndex = 0;
-    childArrayOffset = alignedOffset_8;
-    nodeCounter = 0;
-loop_29:
-    if (nodeCounter < ((cSHierarchy*)alignedOffset)->m_nodeCount)
-    {
-        if (*((s32*)((cSHierarchy*)alignedOffset)->m_childCounts + nodeIndex) > 0)
+        if (hier->m_childCounts[i] > 0)
         {
-            *((s32*)((cSHierarchy*)alignedOffset)->m_childArrays + nodeIndex) = childArrayOffset;
+            hier->m_childArrays[i] = childData;
         }
         else
         {
-            *((s32*)((cSHierarchy*)alignedOffset)->m_childArrays + nodeIndex) = 0;
+            hier->m_childArrays[i] = 0;
         }
-        nodeCounter += 1;
-        temp_r0 = *((s32*)((cSHierarchy*)alignedOffset)->m_childCounts + nodeIndex);
-        nodeIndex += 4;
-        childArrayOffset += temp_r0 * 4;
-        goto loop_29;
+        childData += hier->m_childCounts[i];
     }
-    stackDepth = 0;
-    int stackDepth_ref = stackDepth;
-    BuildPushPopFlags(0, 0, stackDepth_ref);
-    chunkPtr_5 = (void*)((char*)finalChunkPtr + (*(s32*)((char*)finalChunkPtr + 4) + 8));
-    temp_r3_9 = *(s32*)chunkPtr_5 & 0x7F000000;
-    if (((-temp_r3_9 | temp_r3_9) >> 0x1F) != 0)
-    {
-        temp_r4_9 = 1 << (temp_r3_9 >> 0x18);
-        alignedOffset_9 = (s32)((char*)chunkPtr_5 + temp_r4_9 + 7) & ~(temp_r4_9 - 1);
-    }
-    else
-    {
-        alignedOffset_9 = (s32)((char*)chunkPtr_5 + 8);
-    }
-    ((cSHierarchy*)alignedOffset)->m_mirroredNodeIndices = (s32*)alignedOffset_9;
-    nextChunkPtr_4 = (void*)((char*)finalChunkPtr + (*(s32*)((char*)finalChunkPtr + 4) + 8));
-    temp_r3_10 = *(s32*)nextChunkPtr_4 & 0x7F000000;
-    if (((-temp_r3_10 | temp_r3_10) >> 0x1F) != 0)
-    {
-        temp_r4_10 = 1 << (temp_r3_10 >> 0x18);
-        alignedOffset_10 = (s32)((char*)nextChunkPtr_4 + temp_r4_10 + 7) & ~(temp_r4_10 - 1);
-    }
-    else
-    {
-        alignedOffset_10 = (s32)((char*)nextChunkPtr_4 + 8);
-    }
-    ((cSHierarchy*)alignedOffset)->m_translationOffsets = (nlVector3*)alignedOffset_10;
-    chunkPtr_6 = (void*)((char*)nextChunkPtr_4 + (*(s32*)((char*)nextChunkPtr_4 + 4) + 8));
-    temp_r3_11 = *(s32*)chunkPtr_6 & 0x7F000000;
-    if (((-temp_r3_11 | temp_r3_11) >> 0x1F) != 0)
-    {
-        temp_r4_11 = 1 << (temp_r3_11 >> 0x18);
-        alignedOffset_11 = (s32)((char*)chunkPtr_6 + temp_r4_11 + 7) & ~(temp_r4_11 - 1);
-    }
-    else
-    {
-        alignedOffset_11 = (s32)((char*)chunkPtr_6 + 8);
-    }
-    ((cSHierarchy*)alignedOffset)->m_boneLengthFlags = (u8*)alignedOffset_11;
-    return (cSHierarchy*)alignedOffset;
+
+    int stackDepth = 0;
+    hier->BuildPushPopFlags(0, 0, stackDepth);
+
+    chunk = nlGetNextChunk(childDataChunk);
+    hier->m_mirroredNodeIndices = (s32*)nlGetChunkData(chunk);
+
+    chunk = nlGetNextChunk(chunk);
+    hier->m_translationOffsets = (nlVector3*)nlGetChunkData(chunk);
+
+    chunk = nlGetNextChunk(chunk);
+    hier->m_boneLengthFlags = (u8*)nlGetChunkData(chunk);
+
+    return hier;
 }
